eabc187d4bd3ed4800665212a6451c319598fffc
Nastysska <nas.tyu.sha.15.05.06@gmail.com>
2025-05-28 22:06:41 +0300

    publish.sh modified

--- publish.sh
+++ publish.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
-# build
+REPO="git@github-nastia:Nastysska/IO-35_DB_gr4.git"
+
 npm run docs:build
 
-# navigate into the build output directory
 cd docs/.vitepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
+git config user.name Nastysska
+git config user.email nas.tyu.sha.15.05.06@gmail.com
 git branch -M main
 git add -A
 git commit -m 'Deploy to gh-pages'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:boldak/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Nastysska/IO-35_DB_gr4 main:gh-pages
+git push -f "$REPO" main:gh-pages
 
 cd -
